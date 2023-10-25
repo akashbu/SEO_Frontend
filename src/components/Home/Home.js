8bf11a095062360aa1671d0bb2b40a1bed26b06f
Akash Butala <akash.butala.365@gmail.com>
2023-10-25 14:13:34 -0700

    Added post code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { TextField, Button, Container, Typography } from '@mui/material';
 import './Home.css';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 
 const Home = () => {
+
+  const [inputValue, setInputValue] = useState("");
+  const [responseData, setResponseData] = useState(null);
+  const navigate = useNavigate();
+
+
   const customFontStyle = {
     fontFamily: 'cursive, "Comic Sans MS", sans-serif', // Change to your desired font
     fontSize: '45px', // Change to your desired font size
@@ -12,18 +19,62 @@ const Home = () => {
 
   };
 
-  const fetchData = async () => {
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await axios.get('http://127.0.0.1:5000/');
+  //     console.log(response.data);
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+
+  const postString = async () => {
     try {
-      const response = await axios.get('http://127.0.0.1:5000/');
-      console.log(response.data);
+      const response = await axios.post('http://127.0.0.1:5000/', { data: inputValue });
+      setResponseData(response.data);
+      console.log(response.data)
+      localStorage.setItem('responseData', JSON.stringify(response.data));
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error("Error posting string:", error);
     }
   };
+  
+  
+// const handle = () => {
+//   const post_array = [];
+//   post_array.push({
+//     // "location_code": 2840,
+//     "keywords": ["Iphone 12"],
+//     // "date_from": "2021-08-01",
+//     // "search_partners": true 
+//   });
+//     axios({
+//       method: 'post',
+//       url: 'https://api.dataforseo.com/v3/keywords_data/google_ads/search_volume/live',
+//       auth: {
+//         username: 'akbutala@csu.fullerton.edu',
+//         password: 'f99eed1e824dce53'
+//       },
+//       data: post_array,
+//       headers: {
+//         'content-type': 'application/json'
+//       }
+//     }).then(function (response) {
+//       var result = response['data']['tasks'];
+//       // Result data
+//       console.log(result);
+//     }).catch(function (error) {
+//       console.log(error);
+//     });
+    
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+//   }
+
 
   function selectAll(){
     console.log("select all selected");
@@ -50,6 +101,7 @@ const Home = () => {
         <TextField 
           variant="standard"
           size="large"
+          onChange={(e) => setInputValue(e.target.value)}
           InputProps={{
             style: {
               ...customFontStyle, 
@@ -58,38 +110,42 @@ const Home = () => {
           }}
         />
 
-
-  <div class="checkbox-label">
-  <div class="checkbox-label">
-    <input type="checkbox" id="opt5" name="myCheckbox" value="opt5" class="large-checkbox" onChange={()=> selectAll()}/>
-    <label for="opt1"><h3>Select All</h3></label>
+{/* 
+  <div className="checkbox-label">
+  <div className="checkbox-label">
+    <input type="checkbox" id="opt5" name="myCheckbox" value="opt5" className="large-checkbox" onChange={()=> selectAll()}/>
+    <label htmlFor="opt1"><h3>Select All</h3></label>
     </div>
-    <input type="checkbox" id="opt1" name="myCheckbox" value="opt1" class="large-checkbox" />
-    <label for="opt1"><h3>Rabin-Karp</h3></label>
+    <input type="checkbox" id="opt1" name="myCheckbox" value="opt1" className="large-checkbox" />
+    <label htmlFor="opt1"><h3>Rabin-Karp</h3></label>
   </div>
-  <div class="checkbox-label">
-    <input type="checkbox" id="opt2" name="myCheckbox" value="opt2" class="large-checkbox" />
-    <label for="opt2"><h3>Suffix Tree</h3></label>
+  <div className="checkbox-label">
+    <input type="checkbox" id="opt2" name="myCheckbox" value="opt2" className="large-checkbox" />
+    <label htmlFor="opt2"><h3>Suffix Tree</h3></label>
   </div>
-  <div class="checkbox-label">
-    <input type="checkbox" id="opt3" name="myCheckbox" value="opt3" class="large-checkbox" />
-    <label for="opt3"><h3>Suffix Array</h3></label>
+  <div className="checkbox-label">
+    <input type="checkbox" id="opt3" name="myCheckbox" value="opt3" className="large-checkbox" />
+    <label htmlFor="opt3"><h3>Suffix Array</h3></label>
   </div>
-  <div class="checkbox-label">
-    <input type="checkbox" id="opt4" name="myCheckbox" value="opt4" class="large-checkbox" />
-    <label for="opt4"><h3>Naive String Matching</h3></label>
+  <div className="checkbox-label">
+    <input type="checkbox" id="opt4" name="myCheckbox" value="opt4" className="large-checkbox" />
+    <label htmlFor="opt4"><h3>Naive String Matching</h3></label>
 
-    <div class="checkbox-label">
-    <input type="checkbox" id="opt5" name="myCheckbox" value="opt5" class="large-checkbox" />
-    <label for="opt1"><h3>KMP algorithm</h3></label>
+    <div className="checkbox-label">
+    <input type="checkbox" id="opt5" name="myCheckbox" value="opt5" className="large-checkbox" />
+    <label htmlFor="opt1"><h3>KMP algorithm</h3></label>
   </div>
 
-  </div>
+  </div> */}
 
     <Button
      variant="contained"
      color="secondary"
      style={{ marginTop: '20px', fontSize: '1.25rem', padding: '15px 40px' }}
+     onClick={() => {
+      postString();
+      navigate('/about');
+    }}
     >
     ANALYZE
     </Button>
---
+++ src/data.js
@@ -0,0 +1,101 @@
+[
+    {
+        "id": "10250110-6894-0367-0000-2c2dbb4d40f9",
+        "status_code": 20000,
+        "status_message": "Ok.",
+        "time": "5.7173 sec.",
+        "cost": 0.075,
+        "result_count": 1,
+        "path": [
+            "v3",
+            "keywords_data",
+            "google_ads",
+            "search_volume",
+            "live"
+        ],
+        "data": {
+            "api": "keywords_data",
+            "function": "search_volume",
+            "se": "google_ads",
+            "keywords": [
+                "Iphone 12"
+            ]
+        },
+        "result": [
+            {
+                "keyword": "Iphone 12",
+                "spell": "iphone 12",
+                "location_code": null,
+                "language_code": null,
+                "search_partners": false,
+                "competition": "HIGH",
+                "competition_index": 100,
+                "search_volume": 11100000,
+                "low_top_of_page_bid": 0.04,
+                "high_top_of_page_bid": 0.75,
+                "monthly_searches": [
+                    {
+                        "year": 2023,
+                        "month": 9,
+                        "search_volume": 13600000
+                    },
+                    {
+                        "year": 2023,
+                        "month": 8,
+                        "search_volume": 11100000
+                    },
+                    {
+                        "year": 2023,
+                        "month": 7,
+                        "search_volume": 11100000
+                    },
+                    {
+                        "year": 2023,
+                        "month": 6,
+                        "search_volume": 11100000
+                    },
+                    {
+                        "year": 2023,
+                        "month": 5,
+                        "search_volume": 11100000
+                    },
+                    {
+                        "year": 2023,
+                        "month": 4,
+                        "search_volume": 11100000
+                    },
+                    {
+                        "year": 2023,
+                        "month": 3,
+                        "search_volume": 11100000
+                    },
+                    {
+                        "year": 2023,
+                        "month": 2,
+                        "search_volume": 9140000
+                    },
+                    {
+                        "year": 2023,
+                        "month": 1,
+                        "search_volume": 13600000
+                    },
+                    {
+                        "year": 2022,
+                        "month": 12,
+                        "search_volume": 13600000
+                    },
+                    {
+                        "year": 2022,
+                        "month": 11,
+                        "search_volume": 13600000
+                    },
+                    {
+                        "year": 2022,
+                        "month": 10,
+                        "search_volume": 13600000
+                    }
+                ]
+            }
+        ]
+    }
+]
